f66f893ab3700f2632001f598ac8dfadd46fe5a2
trte135 <trte135@naver.com>
2023-04-19 11:16:43 +0900

    feat: first commit - java Project Build

---
+++ src/main/java/com/likelion/project2/PrintHello.java
@@ -0,0 +1,7 @@
+package com.likelion.project2;
+
+public class PrintHello {
+		void print() {
+				System.out.println("Hello");
+		}
+}
---
+++ src/main/java/com/likelion/project2/PrintHelloTest.java
@@ -0,0 +1,8 @@
+package com.likelion.project2;
+
+public class PrintHelloTest {
+		public static void main(String[] args) {
+				PrintHello hello = new PrintHello();
+				hello.print();
+		}
+}
