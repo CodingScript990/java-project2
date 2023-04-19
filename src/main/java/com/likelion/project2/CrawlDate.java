7441afb9715bbc986633c348033d45e829ae3ccb
trte135 <trte135@naver.com>
2023-04-19 13:18:10 +0900

    feat: 마지막 CrawlDate 작업1-1 - Content : Comment Add[LocalDateTime type, CrawlDate constructor]

--- src/main/java/com/likelion/project2/CrawlDate.java
+++ src/main/java/com/likelion/project2/CrawlDate.java
@@ -3,9 +3,10 @@ package com.likelion.project2;
 import java.time.LocalDateTime;
 
 public class CrawlDate {
+		// Set initial value[초기값 설정]
 		private LocalDateTime lastCrawlDateTime;
 
-		// Constructor add
+		// Constructor add[생성자 추가]
 		public CrawlDate(LocalDateTime lastCrawlDateTime) {
 				this.lastCrawlDateTime = lastCrawlDateTime;
 		}
