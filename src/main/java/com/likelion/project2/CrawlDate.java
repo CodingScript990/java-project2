67feb10ac2915586f4b15e2fff26e21155739f6a
trte135 <trte135@naver.com>
2023-04-19 13:10:09 +0900

    feat: 마지막 CrawlDate 작업1 - Content : CrawlDate Domain Add

---
+++ src/main/java/com/likelion/project2/CrawlDate.java
@@ -0,0 +1,12 @@
+package com.likelion.project2;
+
+import java.time.LocalDateTime;
+
+public class CrawlDate {
+		private LocalDateTime lastCrawlDateTime;
+
+		// Constructor add
+		public CrawlDate(LocalDateTime lastCrawlDateTime) {
+				this.lastCrawlDateTime = lastCrawlDateTime;
+		}
+}
