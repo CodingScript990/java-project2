1b3a5625eedd6347851ab68dbcd22d32e62fcdaf
trte135 <trte135@naver.com>
2023-04-19 11:33:40 +0900

    feat: Bye Function Add - Content : ByeClass, ByClassTest Add

---
+++ src/main/java/com/likelion/project2/ByeClass.java
@@ -0,0 +1,7 @@
+package com.likelion.project2;
+
+public class ByeClass {
+		public void print() {
+				System.out.println("Bye");
+		}
+}
---
+++ src/main/java/com/likelion/project2/ByeClassTest.java
@@ -0,0 +1,8 @@
+package com.likelion.project2;
+
+public class ByeClassTest {
+		public static void main(String[] args) {
+				ByeClass bye = new ByeClass();
+				bye.print();
+		}
+}
