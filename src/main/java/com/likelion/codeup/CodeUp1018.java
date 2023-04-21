56b2574687e17db4f1ee05bfff71417d9ee81a16
trte135 <trte135@naver.com>
2023-04-21 16:17:02 +0900

    feat: CodeUp - [1017, 1018] - Content :   - CodeUp1017, CodeUp1018 문제 풀이

---
+++ src/main/java/com/likelion/codeup/CodeUp1018.java
@@ -0,0 +1,14 @@
+package com.likelion.codeup;
+
+import java.util.Scanner;
+
+public class CodeUp1018 {
+		public static void main(String[] args) {
+				Scanner sc = new Scanner(System.in);
+				// Split 으로 : 구분(output)
+				//String time[] = sc.next().split(":");
+				//System.out.printf("%s:%s", time[0], time[1]);
+				// nextLine 으로 output
+				System.out.printf("%s", sc.nextLine());
+		}
+}
