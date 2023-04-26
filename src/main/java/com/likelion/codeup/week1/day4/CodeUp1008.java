0834965a19880b104570243d2bceb30a192d1487
trte135 <trte135@naver.com>
2023-04-26 09:21:58 +0900

    feat: CodeUp - [1006-1015] - Content :   - CodeUp1006   - CodeUp1007   - CodeUp1008   - CodeUp1009   - CodeUp1010   - CodeUp1011   - CodeUp1012   - CodeUp1013   - CodeUp1014   - CodeUp1015

--- src/main/java/com/likelion/codeup/week1/day4/CodeUp1008.java
+++ src/main/java/com/likelion/codeup/week1/day4/CodeUp1008.java
@@ -1,5 +1,5 @@
-package com.likelion.codeup;
-
+package com.likelion.codeup.week1.day4;
+// 2023.4.20
 public class CodeUp1008 {
 		public static void main(String[] args) {
 				System.out.println("\u250c\u252c\u2510\n" +
