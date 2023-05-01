98641b697fd08afad3a6d2f001c72680bbc3e87b
trte135 <trte135@naver.com>
2023-05-01 11:19:08 +0900

    feat: CodeUp - [1071, 1072(Update - Comment)]   - Content :     - CodeUp1071 => Write[Comment(각 역할)]     - CodeUp1072 => Write[Comment(각 역할)]

--- src/main/java/com/likelion/codeup/week3/day11/CodeUp1072.java
+++ src/main/java/com/likelion/codeup/week3/day11/CodeUp1072.java
@@ -5,14 +5,20 @@ import java.util.Scanner;
 public class CodeUp1072 {
 		public static void main(String[] args) {
 
+				// new operator
 				Scanner sc = new Scanner(System.in);
 
+				// int type initial value
 				int inputNum = sc.nextInt();
 
+				// int array type value
 				int[] inputValues = new int[inputNum];
 
+				// for statement
 				for (int i = 0; i < inputNum; i++) {
+						// 대입연산[i] = new operator Object
 						inputValues[i] = sc.nextInt();
+						// output => int type array value[i]
 						System.out.printf("%d\n", inputValues[i]);
 				}
 		}
